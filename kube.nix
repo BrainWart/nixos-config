84b3843ad50bbbe32fcc3b229adef3554780d218
Cody McGinnis <brainwart@gmail.com>
2023-05-25 00:26:14 +0000

    add dns entries and change k3s ip

--- kube.nix
+++ kube.nix
@@ -7,7 +7,7 @@
   services.k3s = {
     enable = true;
     role = "server";
-    extraFlags = "--data-dir /persist/k3s --node-external-ip 100.69.240.109";
+    extraFlags = "--data-dir /persist/k3s --node-external-ip 100.69.240.109 --node-ip 100.69.240.109 --flannel-iface=tailscale0 --advertise-address 100.69.240.109";
   };
   networking.firewall.allowedTCPPorts = [ 6443 443 80 ];
   networking.firewall.enable = lib.mkForce false;
