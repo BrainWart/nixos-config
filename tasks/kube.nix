8ad3a62d21c461e6139ffe070aff36931ecb6e01
Cody McGinnis <cody@mcginnis.dev>
2024-02-01 02:41:24 +0000

    kube settings fixes

--- tasks/kube.nix
+++ tasks/kube.nix
@@ -5,12 +5,21 @@
     "fs.inotify.max_user_instances" = 512;
     "fs.inotify.max_user_watches" = 524288;
   };
+
+  systemd.services.k3s.serviceConfig.KillMode = lib.mkForce "mixed";
   services.k3s = {
     enable = true;
     role = "server";
-    extraFlags = "--data-dir /persist/k3s --node-external-ip 100.69.240.109 --node-ip 100.69.240.109 --flannel-iface=tailscale0 --advertise-address 100.69.240.109";
+    extraFlags = toString [
+      "--data-dir /persist/k3s"
+      "--node-external-ip 100.69.240.109"
+      "--node-ip 100.69.240.109"
+      "--flannel-iface=tailscale0"
+      "--advertise-address 100.69.240.109"
+    ];
   };
+
   networking.firewall.allowedTCPPorts = [ 6443 443 80 ];
-  networking.firewall.enable = lib.mkForce false;
+  networking.firewall.allowedUDPPorts = [ 8472 ];
 })
 
