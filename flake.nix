4acbe4087f848b846a63081623792ae1e02b742f
Cody McGinnis <cody@mcginnis.dev>
2024-02-24 14:23:16 +0000

    voice-assistant: disable piper

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
 
               modules = [
                 ({ networking.hostName = "nixos"; })
+                ({ nix.settings = { max-jobs = 2; cores = 8; }; })
                 ./providers/pve.nix
                 ./tasks/tailscale.nix
               ];
@@ -55,11 +56,49 @@
               ];
             };
 
-            voice-assistant = nixpkgs.lib.nixosSystem {
+            voice-assistant = let
+              pkgs = (import nixpkgs { inherit system; config.allowUnfree = true; });
+            in nixpkgs.lib.nixosSystem {
               inherit system;
 
               modules = [
-                ({ networking.hostName = "voice-assistant"; })
+                ({
+                  networking.hostName = "voice-assistant";
+                  nixpkgs.config.allowUnfree = true;
+                  # nixpkgs.config.cudaSupport = true;
+
+                  services.xserver.videoDrivers = ["nvidia"];
+
+                  hardware.opengl.enable = true;
+                  hardware.nvidia = {
+                    modesetting.enable = true;
+                    open = false;
+                    nvidiaSettings = true;
+                    nvidiaPersistenced = true;
+                    # package = pkgs.kernelPackages.nvidiaPackages.stable;
+                  };
+
+                  environment.systemPackages = [
+                    # pkgs.cudatoolkit
+                  ];
+
+                  nixpkgs.overlays = [
+                    (_: prev: {
+                      ctranslate2 = prev.ctranslate2.override {
+                        withCUDA = true;
+                        withCuDNN = true;
+                        stdenv = prev.gcc12Stdenv;
+                      };
+                      python3 = prev.python3.override {
+                        packageOverrides = (_: pythonPrev: {
+                          torch = pythonPrev.torch.override {
+                            cudaSupport = true;
+                          };
+                        });
+                      };
+                    })
+                  ];
+                })
                 ./providers/pve.nix
                 ./tasks/tailscale.nix
                 ./tasks/voice-assistant.nix
--- tasks/voice-assistant.nix
+++ tasks/voice-assistant.nix
@@ -1,12 +1,11 @@
-
 ({ config, pkgs, modulesPath, lib, ... }:
 {
   services.wyoming.piper = {
     servers = {
       "en" = {
-        enable = true;
+        enable = false;
         # see https://github.com/rhasspy/rhasspy3/blob/master/programs/tts/piper/script/download.py
-        voice = "en-gb-southern_english_female-low";
+        voice = "en_US-amy-medium";
         uri = "tcp://0.0.0.0:10200";
         speaker = 0;
       };
@@ -18,18 +17,22 @@
       "en" = {
         enable = true;
         # see https://github.com/rhasspy/rhasspy3/blob/master/programs/asr/faster-whisper/script/download.py
-        model = "tiny-int8";
+        model = "small-int8";
         language = "en";
         uri = "tcp://0.0.0.0:10300";
-        device = "auto";
+        device = "cuda";
       };
     };
   };
 
   # needs access to /proc/cpuinfo
-  systemd.services."wyoming-faster-whisper-en".serviceConfig.ProcSubset = lib.mkForce "all";
+  systemd.services."wyoming-faster-whisper-en" = {
+    serviceConfig.ProcSubset = lib.mkForce "all";
+    after = lib.mkForce [ "network.target" ];
+  };
 
-  systemd.services."wyoming-faster-whisper-en".after = lib.mkForce [ "network.target" ];
   systemd.services."wyoming-piper-en".after = lib.mkForce [ "network.target" ];
+
+  networking.firewall.allowedTCPPorts = [ 10300 10200 ];
 })
 
