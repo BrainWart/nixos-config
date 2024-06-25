e6d4c2ccb5fd68db2e9547cb702db6988dd97089
Cody McGinnis <cody@mcginnis.dev>
2024-06-25 07:19:46 +0000

    do more install

--- tasks/install.nix
+++ tasks/install.nix
@@ -6,7 +6,7 @@ let
 
     if [ -e /dev/disk/by-label/cidata ] ; then
       mkdir -p /tmp/cidata
-      ${mount}/bin/mount /dev/disk/by-label/cidata /tmp/cidata
+      ${mount}/bin/mount -r /dev/disk/by-label/cidata /tmp/cidata
       HOSTNAME=$(${gnused}/bin/sed -ne '/hostname:/s/hostname: \(.*\)/\1/p' /tmp/cidata/user-data)
     fi
 
@@ -14,7 +14,7 @@ let
       HOSTNAME="$(${systemd}/bin/systemd-ask-password --timeout=0 "Hostname:")"
     fi
 
-    DISKO="$(nix build ${config.system.autoUpgrade.flake}#nixosConfigurations.''${HOSTNAME}.config.system.build.diskoScript --print-out-paths)"
+    DISKO="$(${nix}/bin/nix build ${config.system.autoUpgrade.flake}#nixosConfigurations.''${HOSTNAME}.config.system.build.diskoScript --print-out-paths)"
     $DISKO
     ${nixos-install-tools}/bin/nixos-install --flake ${config.system.autoUpgrade.flake}#$HOSTNAME --no-root-password
     reboot
