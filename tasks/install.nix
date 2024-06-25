f07d8f0bb06af0d0073e3354bbffd29db2a77d42
Cody McGinnis <cody@mcginnis.dev>
2024-06-25 07:34:15 +0000

    fix paths?

--- tasks/install.nix
+++ tasks/install.nix
@@ -1,25 +1,5 @@
 ({ config, pkgs, modulesPath, lib, ... }:
-let
-  builder = with pkgs; writeScript "buildSystem" ''
-    set -e
-    sleep 10
-
-    if [ -e /dev/disk/by-label/cidata ] ; then
-      mkdir -p /tmp/cidata
-      ${mount}/bin/mount -r /dev/disk/by-label/cidata /tmp/cidata
-      HOSTNAME=$(${gnused}/bin/sed -ne '/hostname:/s/hostname: \(.*\)/\1/p' /tmp/cidata/user-data)
-    fi
-
-    if [ -z "$HOSTNAME" ] ; then
-      HOSTNAME="$(${systemd}/bin/systemd-ask-password --timeout=0 "Hostname:")"
-    fi
-
-    DISKO="$(${nix}/bin/nix build ${config.system.autoUpgrade.flake}#nixosConfigurations.''${HOSTNAME}.config.system.build.diskoScript --print-out-paths)"
-    $DISKO
-    ${nixos-install-tools}/bin/nixos-install --flake ${config.system.autoUpgrade.flake}#$HOSTNAME --no-root-password
-    reboot
-  '';
-in {
+{
   systemd.services.autoinstall = {
     description = "Automatic installation";
 
@@ -28,7 +8,26 @@ in {
     wantedBy = [ "multi-user.target" ];
     requires = [ "getty@tty1.service" ];
 
-    script = "${builder}";
+    path = [ pkgs.nix pkgs.nixos-install-tools ];
+
+    script = ''
+      sleep 5
+
+      if [ -e /dev/disk/by-label/cidata ] ; then
+        mkdir -p /tmp/cidata
+        ${mount}/bin/mount -r /dev/disk/by-label/cidata /tmp/cidata
+        HOSTNAME=$(${gnused}/bin/sed -ne '/hostname:/s/hostname: \(.*\)/\1/p' /tmp/cidata/user-data)
+      fi
+
+      if [ -z "$HOSTNAME" ] ; then
+        HOSTNAME="$(${systemd}/bin/systemd-ask-password --timeout=0 "Hostname:")"
+      fi
+
+      DISKO="$(nix build ${config.system.autoUpgrade.flake}#nixosConfigurations.''${HOSTNAME}.config.system.build.diskoScript --print-out-paths)"
+      $DISKO
+      nixos-install --flake ${config.system.autoUpgrade.flake}#$HOSTNAME --no-root-password
+      reboot
+    '';
   };
 })
 
