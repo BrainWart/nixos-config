7a4058a3fa6c127a2001ea0b442b0db846b3bfb0
Cody McGinnis <brainwart@gmail.com>
2024-07-28 10:27:17 -0400

    moar fix

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -6,29 +6,29 @@
   ];
 
   networking.hostName = "nixos";
-  nixpkgs.config.allowUnfree = true;
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport32Bit = true;
+  # nixpkgs.config.allowUnfree = true;
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.production;
     modesetting.enable = true;
     open = false;
     nvidiaSettings = true;
   };
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-  services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
-  users.users.mcginnisc.linger = true;
-  services.xrdp = {
-    enable = true;
-    defaultWindowManager = "startplasma-x11";
-    extraConfDirCommands = ''
-      substituteInPlace $out/xrdp.ini --replace port=-1 port=ask-1
-    '';
-  };
-  environment.systemPackages = [
-    pkgs.firefox
-  ];
+  # services.xserver.enable = true;
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.displayManager.lightdm.enable = true;
+  # users.users.mcginnisc.linger = true;
+  # services.xrdp = {
+  #   enable = true;
+  #   defaultWindowManager = "startplasma-x11";
+  #   extraConfDirCommands = ''
+  #     substituteInPlace $out/xrdp.ini --replace port=-1 port=ask-1
+  #   '';
+  # };
+  # environment.systemPackages = [
+  #   pkgs.firefox
+  # ];
 }
 
