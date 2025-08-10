a28f746a4bf2bf2aaa2dde65e5d49c82281442c5
Cody McGinnis <cody@mcginnis.dev>
2025-08-10 14:29:07 +0000

    more changes

--- tasks/mail-forwarding.nix
+++ tasks/mail-forwarding.nix
@@ -8,9 +8,11 @@
       mcginnis.dev   smtp:[smtp-relay.gmail.com]:465
       *              discard:
     '';
-    submissionOptions = {
+    enableSubmissions = true;
+    submissionsOptions = {
       smtp_sasl_auth_enable = "no";
       smtp_tls_security_level = "encrypt";
+      smtp_tls_wrappermode = "yes";
     };
   };
 
