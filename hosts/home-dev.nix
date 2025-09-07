26056568626e3a28a24ceaf64ac2192ea9d74cc1
Cody McGinnis <cody@mcginnis.dev>
2025-09-07 20:35:25 +0000

    temp add code-server

--- hosts/home-dev.nix
+++ hosts/home-dev.nix
@@ -10,6 +10,51 @@
 
   networking.hostName = "home-dev";
 
+  users.users.nginx.extraGroups = [ "acme" ];
+  
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      server = "https://stepca.dev.mcginnis.internal/acme/acme/directory";
+      email = "cody@mcginnis.dev";
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."home-dev.mcginnis.internal" =  {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        root = "/var/www";
+      };
+      locations."/ide/" = {
+        proxyPass = "http://127.0.0.1:4444/";
+        proxyWebsockets = true; # needed if you need to use WebSocket
+        extraConfig =
+          # required when the target is also TLS server with multiple hosts
+          "proxy_ssl_server_name on;" +
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+          ;
+      };
+    };
+  };
+
+  services.code-server = {
+    enable = true;
+    user = "mcginnisc";
+    group = "users";
+    disableUpdateCheck = true;
+    disableTelemetry = true;
+    host = "127.0.0.1";
+    port = 4444;
+    hashedPassword = "$argon2i$v=19$m=4096,t=3,p=1$aWF2WCs4UTlidzNaQTFlQytRVlRpaE1zeEZjPQ$a49d0Q5wN/v7cGFcIl9DaSGvtS8PKiLz5pfYU+HzgYY";
+  };
+  networking.firewall.allowedTCPPorts = [ 80 443 4444 ];
+
   # services.xserver.desktopManager.plasma5.enable = true;
   # services.xserver.displayManager.lightdm.enable = true;
   # users.users.mcginnisc.linger = true;
